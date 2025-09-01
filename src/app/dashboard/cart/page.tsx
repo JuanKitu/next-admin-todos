935883d0563a82bfae16d4437de7bc27e6042242
kitu <juanmix.01@gmail.com>
2025-09-01 00:37:10 -0300

    Add shopping cart page and `ItemCard` component, implement cookie-based cart item management, and enhance `TopMenu` with cart navigation.

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -3,6 +3,7 @@ import {Metadata} from "next";
 import {cookies} from "next/headers";
 import {Product, products} from "@/products/data/products";
 import {ItemCard} from "@/shopping-cart";
+import {WidgetItem} from "@/app/components";
 export const metadata: Metadata = {
     title: 'Carrito de compras',
     description: 'Carrito de compras'
@@ -28,17 +29,29 @@ export default async function CartPage() {
     const cookiesStore = await cookies();
     const cart = JSON.parse(cookiesStore.get('cart')?.value ?? '{}' ) as {[id: string]: number};
     const productsInCart = getProductInCart(cart);
+    const totalToPay = productsInCart.reduce((prev, current) => ( current.product.price * current.quantity ) + prev, 0);
     return (
         <div>
             <h1 className="text-5xl">Productos del carrito</h1>
             <hr className="mb-2" />
-            <div className="flex flex-col sm: gap-2 w-full">
+            <div className="flex flex-col sm:flex-row gap-2 w-full">
                 <div className="flex flex-col gap-2 w-full sm:w-8/12">
                     {
                         productsInCart.map(({product, quantity}) => (
                             <ItemCard key={product.id} product={product} quantity={quantity} />
                         ))
                     }
+                </div>
+                <div className="flex flex-col w-full sm:w-4/12">
+                    <WidgetItem title="Total a pagar">
+                        <div className="mt-2 flex justify-center gap-4">
+                            <h3 className="text-3xl font-bold text-gray-700">{(totalToPay * 1.15).toFixed(2)}</h3>
+                        </div>
+                        <span className="font-bold text-center text-gray-500">
+                            Impuestos del 15%: ${(totalToPay * 0.15).toFixed(2)}
+                        </span>
+                    </WidgetItem>
+
                 </div>
             </div>
         </div>
