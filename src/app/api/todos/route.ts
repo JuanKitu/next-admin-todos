e29b2063ad93d2479bb6d636a2a98432b6b0f0f0
kitu <juanmix.01@gmail.com>
2025-09-04 01:38:51 -0300

    Add user session validation in `POST /todos`, refactor code style for consistency, and update Prisma `todo` creation to include `userId`.

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,51 +1,50 @@
-
 import prisma from '@/lib/prisma'
-import { NextResponse } from 'next/server'
+import {NextResponse} from 'next/server'
 import * as yup from 'yup';
+import {getUserServerSession} from "@/auth/actions/auth.actions";
 
-export async function GET(request: Request) { 
-  
-  const { searchParams } = new URL(request.url)
-  const take = Number( searchParams.get('take') ?? '10');
-  const skip = Number( searchParams.get('skip') ?? '0');
+export async function GET(request: Request) {
 
-  if ( isNaN(take) ){ 
-    return NextResponse.json({ message: 'Take tiene que ser un número' }, { status: 400 });
-  }
+    const {searchParams} = new URL(request.url)
+    const take = Number(searchParams.get('take') ?? '10');
+    const skip = Number(searchParams.get('skip') ?? '0');
 
-  if ( isNaN(skip) ){ 
-    return NextResponse.json({ message: 'Skip tiene que ser un número' }, { status: 400 });
-  }
+    if (isNaN(take)) {
+        return NextResponse.json({message: 'Take tiene que ser un número'}, {status: 400});
+    }
 
-  const todos = await prisma.todo.findMany({
-    take: take,
-    skip: skip,
-  });
+    if (isNaN(skip)) {
+        return NextResponse.json({message: 'Skip tiene que ser un número'}, {status: 400});
+    }
 
-  return NextResponse.json( todos );
+    const todos = await prisma.todo.findMany({
+        take: take,
+        skip: skip,
+    });
+
+    return NextResponse.json(todos);
 }
-
 
 
 const postSchema = yup.object({
-  description: yup.string().required(),
-  complete: yup.boolean().optional().default(false),
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false),
 });
 
-export async function POST(request: Request) { 
+export async function POST(request: Request) {
 
-  try {
-    const { complete, description } = await postSchema.validate( await request.json() );
+    try {
+        const user = await getUserServerSession();
+        if (!user) return NextResponse.json('No autorizado', {status: 401});
+        const {complete, description} = await postSchema.validate(await request.json());
+        const todo = await prisma.todo.create({data: {complete, description, userId: user.id}})
+        return NextResponse.json(todo);
 
-    const todo = await prisma.todo.create({ data: { complete, description } })
-  
-    
-    return NextResponse.json(todo);
-    
-  } catch (error) {
-    return NextResponse.json( error, { status: 400 } );
-  }
+    } catch (error) {
+        return NextResponse.json(error, {status: 400});
+    }
 }
+
 export async function DELETE() {
 
     try {
@@ -57,6 +56,6 @@ export async function DELETE() {
         return NextResponse.json(todo);
 
     } catch (error) {
-        return NextResponse.json( error, { status: 400 } );
+        return NextResponse.json(error, {status: 400});
     }
 }
